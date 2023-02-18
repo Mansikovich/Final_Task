329ca265253224a8cff47ba0841848d465dced0f
Mansur Dagaev <mans.4e4en@mail.ru>
2023-02-18 10:59:10 +0300

    1

---
+++ Блок-схема/код/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+
+string[] Input = new string[11] {"hello", "2", "world",":-)", "1234", "1567", 
+"-2", "computer science","Russia","Denmark","Kazan"};
+string[] Output = new string[Input.Length];
+FillingOutputArray(Input, Output);
+PrintArray(Output);
+
+void FillingOutputArray(string[] Input, string[] Output)
+{
+    int count = 0;
+    for (int i = 0; i < Input.Length; i++)
+    {
+    if(Input[i].Length <= 3)
+        {
+        Output[count] = Input[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+    }
+}
